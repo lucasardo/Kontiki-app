38d9394426213c0b71e39ee4bb0b64f3d659db4f
Luca Sardo <77811564+lucasardo@users.noreply.github.com>
2024-05-08 22:03:59 +0200

    Update kt-app.py

--- kt-app.py
+++ kt-app.py
@@ -8,14 +8,14 @@ from llama_index.embeddings.mistralai import MistralAIEmbedding
 
 mistral_key = st.secrets["mistral_key"]
 
+# Define LLM and embedding model
+Settings.llm = MistralAI(model="mistral-small", api_key=mistral_key)
+Settings.embed_model = MistralAIEmbedding(model_name='mistral-embed', api_key=mistral_key)
+
 # rebuild storage context
 storage_context = StorageContext.from_defaults(persist_dir="Store")
 index = load_index_from_storage(storage_context, embed_model=MistralAIEmbedding(model_name='mistral-embed', api_key=mistral_key))
 query_engine = index.as_query_engine(similarity_top_k=2)
-
-# Define LLM and embedding model
-Settings.llm = MistralAI(model="mistral-small", api_key=mistral_key)
-Settings.embed_model = MistralAIEmbedding(model_name='mistral-embed', api_key=mistral_key)
 
 ################################################################################################################
 
